60692e89575d34d1d87a2c92b5161d3fb95457ad
Julian Moschüring <archyinf_github@gmx.com>
2025-05-11 01:13:55 +0200

    zoom focuses random face instead of face center

--- immichFrame.Web/src/lib/components/elements/image.svelte
+++ immichFrame.Web/src/lib/components/elements/image.svelte
@@ -40,10 +40,19 @@
 	let naturalWidth = $state(0);
 	let naturalHeight = $state(0);
 	let scale = $state(1.0);
+	let focusFace: any = $state(null);
 
 	let hasPerson = $derived(image[1].people?.filter((x) => x.name).length ?? 0 > 0);
 
 	function GetBounds(face: any, scale: number) {
+		if (face == null) {
+			return {
+				X1: 0,
+				Y1: 0,
+				X2: 0,
+				Y2: 0
+			};
+		}
 		let x1 = face.boundingBoxX1 ?? 0;
 		let x2 = face.boundingBoxX2 ?? 0;
 		let y1 = face.boundingBoxY1 ?? 0;
@@ -52,7 +61,12 @@
 		const centerY = (y1 + y2) / 2;
 		const width = (x2 - x1) / 2;
 		const height = (y2 - y1) / 2;
-		let bounds = {X1: centerX - width * scale, Y1: centerY - height * scale, X2: centerX + width * scale, Y2: centerY + height * scale};
+		let bounds = {
+			X1: centerX - width * scale,
+			Y1: centerY - height * scale,
+			X2: centerX + width * scale,
+			Y2: centerY + height * scale
+		};
 		bounds.X1 = Math.max(bounds.X1, 0);
 		bounds.Y1 = Math.max(bounds.Y1, 0);
 		bounds.X2 = Math.min(bounds.X2, naturalWidth);
@@ -75,10 +89,31 @@
 				faceBoundsX1 = Math.min(bounds.X1, faceBoundsX1);
 				faceBoundsY1 = Math.min(bounds.Y1, faceBoundsY1);
 				faceBoundsX2 = Math.max(bounds.X2, faceBoundsX2);
-				faceBoundsY2 = Math.max(bounds.Y2, faceBoundsY2);	
+				faceBoundsY2 = Math.max(bounds.Y2, faceBoundsY2);
 			}
 		}
 		return {X1: faceBoundsX1, Y1: faceBoundsY1, X2: faceBoundsX2, Y2: faceBoundsY2};
+	}
+
+	function GetRandomFace() {
+        let persons = image[1].people as PersonWithFacesResponseDto[];
+        persons = persons.filter((x) => x.name);
+        
+        let count = 0;
+        for (let i = 0; i < persons.length; i++) {
+            count += persons[i].faces.length;
+        }
+        let index = Math.floor(Math.random() * count);
+        
+        count = 0;
+		for (let i = 0; i < persons.length; i++) {
+			for (let j = 0; j < persons[i].faces.length; j++) {
+				if (count == index)
+					return persons[i].faces[j];
+				count++;
+			}
+		}
+		return null;
 	}
 
 	onMount(() => {
@@ -101,8 +136,7 @@
 				} else {
 					scale = wrapperWidth / naturalWidth;
 				}
-			}
-			else {
+			} else {
 				if (aspectImg > aspectDiv) {
 					scale = wrapperWidth / naturalWidth;
 				} else {
@@ -135,18 +169,18 @@
 
 			if (visibleX1 > faceBounds.X1) {
 				centerX -= visibleX1 - faceBounds.X1;
-			}
-			else if (visibleX2 < faceBounds.X2) {
+			} else if (visibleX2 < faceBounds.X2) {
 				centerX += faceBounds.X2 - visibleX2;
 			}
 
 			if (visibleY1 > faceBounds.Y1) {
 				centerY -= visibleY1 - faceBounds.Y1;
-			} 
-			else if (visibleY2 < faceBounds.Y2) {
+			} else if (visibleY2 < faceBounds.Y2) {
 				centerY += faceBounds.Y2 - visibleY2;
 			}
 		}
+
+		focusFace = GetRandomFace();
 
 		updateImageMetrics();
 		imgEl.addEventListener('load', updateImageMetrics);
@@ -177,11 +211,11 @@
 	<img
 		bind:this={imgEl}
 		style="
-		    --interval: {interval + 2}s;
-		    --focusX: {scale*(GetFacesBounds().X2+GetFacesBounds().X1)/2}px;
-		    --focusY: {scale*(GetFacesBounds().Y2+GetFacesBounds().Y1)/2}px;
-		    --centerX: {-centerX*scale + wrapperWidth/2}px;
-		    --centerY: {-centerY*scale + wrapperHeight/2}px;
+			--interval: {interval + 2}s;
+			--focusX: {scale*(GetBounds(focusFace, 1).X2+GetBounds(focusFace, 1).X1)/2}px;
+			--focusY: {scale*(GetBounds(focusFace, 1).Y2+GetBounds(focusFace, 1).Y1)/2}px;
+			--centerX: {-centerX*scale + wrapperWidth/2}px;
+			--centerY: {-centerY*scale + wrapperHeight/2}px;
 			max-width: none;
 			width: {naturalWidth*scale}px; height: {naturalHeight*scale}px;
 			transform: translate({-centerX*scale + wrapperWidth/2}px, {-centerY*scale + wrapperHeight/2}px);
